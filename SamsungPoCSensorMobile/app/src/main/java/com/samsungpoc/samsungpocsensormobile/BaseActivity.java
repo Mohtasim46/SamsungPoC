4345b7a90c8e806f0fb5879947b9115ff018862c
Mohtasim Bellah <mohtasim22@gmail.com>
2020-07-09 02:22:56 +0600

    Sensor: Implemented step count to get real step count data.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

--- SamsungPoCSensorMobile/app/src/main/java/com/samsungpoc/samsungpocsensormobile/BaseActivity.java
+++ SamsungPoCSensorMobile/app/src/main/java/com/samsungpoc/samsungpocsensormobile/BaseActivity.java
@@ -1,16 +1,30 @@
 package com.samsungpoc.samsungpocsensormobile;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
+    protected String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayout());
         init();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
     }
 
     protected abstract int getLayout();
